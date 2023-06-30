9ee824cb946bae23f9d3d1a6e5ca93eee03f39cb
ProfHastings <philipzeiler@icloud.com>
2023-06-30 17:51:04 +0200

    removing deprecated code

--- api.py
+++ api.py
@@ -14,7 +14,6 @@ from langchain.schema import (
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import asyncio
 from langchain.embeddings import OpenAIEmbeddings
-import gc
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -32,7 +31,6 @@ gpt35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, max_tokens=5, max_
 gptdataquery = ChatOpenAI(model_name="gpt-4", temperature=0, max_tokens=256)
 tokenizer = tiktoken.encoding_for_model("gpt-4")
 
-
 #template for final analysis prompt
 analysis_system_message = SystemMessage(content="Du bist ein im österreichischen Recht erfahrener Anwalt.")
 analysis_template_string = """
@@ -48,17 +46,6 @@ Schließlich gib an, wie die Frage deines Erachtens zu lösen ist. Gib immer auc
 Zum Schluss liste bis zu fünf der wichtigsten Entscheidungen und bis zu fünf der wichtigsten Literaturzitate auf, die du in den Entscheidungen findest.
 """
 analysis_template = PromptTemplate.from_template(analysis_template_string)
-
-#template for pruning prompt
-pruning_system_message = SystemMessage(content="Du bist ein im österreichischen Recht erfahrener Anwalt. Deine Antwort besteht immer nur aus einem Wort")
-pruning_template_string = """
-Deine Aufgabe ist es zu evaluieren ob ein Abschnitt einer Gerichtsentscheidung relevant sein könnte um die folgende Frage zu beantworten: 
-"{question}"
-Der Abschnitt lautet:
-"{case}"
-Falls du dir sicher bist, dass der Abschnitt die Rechtsfrage nicht betrifft, antworte mit Nein. Ansonsten antworte mit Ja.
-"""
-pruning_template = PromptTemplate.from_template(pruning_template_string)
 
 #template for ranking prompt
 ranking_system_message = SystemMessage(content="Du bist ein im österreichischen Recht erfahrener Anwalt. Deine Antwort besteht immer nur aus einer Zahl von 1 bis 10")
@@ -78,24 +65,6 @@ Generate a paragraph of a court decision from the U.S. Supreme Court Database in
 "{question}"
 Do not mention the US supreme court and write as if this is a real case
 """
-#dataquery_system_message = SystemMessage(content="Du bist ein im österreichischen Recht erfahrener Anwalt. Du antwortest nur genau mit dem was von dir gefragt ist und ausführlich.")
-#dataquery_template_string = """
-#Ein Klient kommt zu dir mit der folgenden Frage.
-#"{question}"
-#Schreibe eine Liste mit den wichtigsten rechtlichen Fragen die sich zu dieser Situation stellen. Verwende die genaue juristische Terminologie.
-#"""
-#dataquery_system_message = SystemMessage(content="Du bist ein im österreichischen Recht erfahrener Anwalt. Du schreibst nur die Antwort auf Fragen.")
-#dataquery_template_string = """
-#Du hast die folgende informell formulierte Frage:"
-#"{question}" 
-#Formuliere die Frage ausführlich um, so wie sie in einer gerichtlichen Entscheidung stehen würde.
-#"""
-#dataquery_system_message = SystemMessage(content="Du bist ein im österreichischen Recht erfahrener Anwalt. Du antwortest nur genau mit dem was von dir gefragt ist und ausführlich.")
-#dataquery_template_string = """
-#Ein Klient kommt zu dir mit der folgenden Frage.
-#"{question}"
-#Identifiziere die rechtlichen Parteien in diesem Fall und nutze dies um eine Liste mit den drei wichtigsten Fragen die sich zu dieser Situation zu schreiben. Nenne die Parteien innerhalt der Liste nur mit ihren rechtlichen Rollen ohne persönliche Namen und schreibe nur die Elemente der Liste ohne Erläuterung. Jedes Element der Liste sollte formuliert sein um die Parteien und Rechtsinteraktion zu referenzieren. Jeder der Punkte soll verständlich sein ohne die Originalfrage gelesen zu haben und die Rechtslage enthalten. Verwende die genaue juristische Terminologie.
-#"""
 
 dataquery_template = PromptTemplate.from_template(dataquery_template_string)
 
@@ -133,24 +102,6 @@ def get_retriever():
     retriever = PineconeHybridSearchRetriever(embeddings=dense_encoder, sparse_encoder=sparse_encoder, index=index, top_k=75, alpha=0.99899) #lower alpha - more sparse
     return retriever
 
-#(next three functions) uses async api calls to prune cases based on relevance
-async def async_prune(case, question):
-    pruning_userprompt = pruning_template.format(case=case.page_content, question=question)
-    pruning_user_message = HumanMessage(content=pruning_userprompt)
-    relevance = await gpt35._agenerate([pruning_system_message, pruning_user_message])
-    print(case.page_content, "\n", relevance.generations[0].text)
-    return (case, relevance.generations[0].text)
-
-async def prune_concurrently(cases, question):
-    tasks = [async_prune(case, question) for case in cases]
-    results = await asyncio.gather(*tasks)
-    #print(results)
-    return [case for case, relevance in results if relevance == 'Ja.']
-
-def prune_cases(results, question):
-    pruned_results = asyncio.run(prune_concurrently(results, question))
-    return pruned_results
-
 #(next three functions) uses async api calls to rank chunks from 1-10 based on relevance
 async def async_rank(case, question, max_attempts=5):
     for attempt in range(max_attempts):
@@ -173,11 +124,6 @@ async def rank_concurrently(cases, question):
     sum_of_relevance = sum(relevance for _, relevance in results)
     return [case for case, _ in sorted_results], sum_of_relevance
 
-def rank_cases(results, question):
-    ranked_results, sum_of_relevance = asyncio.run(rank_concurrently(results, question))
-    print(f"Average relevance score: {sum_of_relevance/len(ranked_results)}")
-    return ranked_results
-
 #rephrases query as optimized prompt for searching vectorstorage
 def get_dataquery(question):
     dataquery_userprompt = dataquery_template.format(question=question)
@@ -185,24 +131,6 @@ def get_dataquery(question):
     dataquery = gptdataquery([dataquery_system_message, dataquery_user_message])
     print(f"Looking in database for: {dataquery.content}")
     return dataquery.content
-
-#attempt to force garbace collection. seems unsuccessful
-def smart_retriever(question):
-    index = get_index()
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dense_encoder = OpenAIEmbeddings(model="text-embedding-ada-002")
-    sparse_encoder = SpladeEncoder(device=device)
-    retriever = PineconeHybridSearchRetriever(embeddings=dense_encoder, sparse_encoder=sparse_encoder, index=index, top_k=75, alpha=0.99899) #lower alpha - more sparse
-    
-    dataquery = get_dataquery(question)
-    print(f"Looking in database for: {dataquery}")  
-
-    results = retriever.get_relevant_documents(dataquery)
-    del (dense_encoder, sparse_encoder, index, device, retriever, dataquery)
-    torch.cuda.empty_cache()
-    gc.collect()
-    return results
 
 async def main(question, streamhandler, queue):
     print("main has started execution")
@@ -213,12 +141,7 @@ async def main(question, streamhandler, queue):
     dataquery = get_dataquery(question)
     results = retriever.get_relevant_documents(dataquery)
     print(f"Found this many cases: {len(results)}")
-    #results = smart_retriever(question)
-    #gc.collect()
-    #for result in results:
-    #    print (result.page_content, "\n", "\n")
-    #return
-    #print(f"{len(results)} chunks found in database")
+
     results, sum_of_relevance = await rank_concurrently(results, question)  # Replaced rank_cases with rank_concurrently
 
     print(f"Average relevance score: {sum_of_relevance/len(results)}")
@@ -230,7 +153,6 @@ async def main(question, streamhandler, queue):
     print(analysis_userprompt)
     user_message = HumanMessage(content=analysis_userprompt)
     gpt4analysis = ChatOpenAI(model_name="gpt-4", temperature=0, max_tokens=2048, streaming=True, callbacks=[streamhandler])
-    #await queue.put("test2")
     try:
         response = await gpt4analysis.agenerate([[analysis_system_message, user_message]])
     except Exception as e:
@@ -239,7 +161,6 @@ async def main(question, streamhandler, queue):
     await queue.put("TABALUGA_WARTET")
     text_output = response.generations[0][0].text
     print(text_output)
-    #await queue.put("test3")
     return text_output
 
 class Item(BaseModel):
